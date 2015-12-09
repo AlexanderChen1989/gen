fc939488deb2cbd27db6c1a52b037dd1adde57e5
Alex Chen <alexanderchenmh@gmail.com>
2015-12-09 13:52:20 +0800

    rm code

--- example/main.go
+++ example/main.go
@@ -1,18 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/AlexanderChen1989/gen/kv"
-)
-
 func main() {
-	kvs := kv.NewKVServer()
-
-	kvs.Put("Hello", "World")
-	kvs.Put("Good", "Body")
-	fmt.Println(kvs.Get("Hello"))
-	fmt.Println(kvs.Get("Good"))
-
-	<-kvs.Stop()
 }
